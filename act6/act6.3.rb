5b01931dd261fb2b57291fcfc53498bd1729ec4f
AdrianIlaga <adrianilaga771@gmail.com>
2021-07-07 16:48:58 +0800

    Made corrections to Liskov principle act

--- act6/act6.3.rb
+++ act6/act6.3.rb
@@ -1,59 +1,71 @@
 # The Bad
 
-class Father
-    
-    def father_job
-        puts "Take care of children"
+class FamilyMember
+    def job
+        puts "Go to job"
     end
-
 end
 
-class Eldest < Father
-
-    def eldest_job
-        puts "Look after siblings"
-    end
-
+class Father < FamilyMember
 end
 
-class Youngest < Father
-
-    def youngest_job
-        puts "Be annoying"
-    end
-
+class Eldest < FamilyMember
 end
 
-Father.new.father_job
-Eldest.new.eldest_job
-Youngest.new.youngest_job
+class Youngest < FamilyMember
+    def job
+        raise "Cannot have job yet"
+    end
+end
+
+father = Father.new
+eldest = Eldest.new
+youngest = Youngest.new
+
+def show_job(member)
+    member.job
+end 
+
+show_job(father)
+show_job(eldest)
+show_job(youngest)
 
 # The Good (Hopefully)
 
-class Father2
-    
+class WorkingFamilyMember
     def job
-        puts "Take care of children"
+        puts "Go to Job"
     end
-
 end
 
-class Eldest2 < Father2
-
-    def job
-        puts "Look after siblings"
+class DependentFamilyMember
+    def chore
+        puts "Do Chores"
     end
-
 end
 
-class Youngest2 < Father2
-
-    def job
-        puts "Be annoying"
-    end
-
+class Father2 < WorkingFamilyMember
 end
 
-Father2.new.job
-Eldest2.new.job
-Youngest2.new.job
+class Eldest2 < WorkingFamilyMember
+end
+
+class Youngest2 < DependentFamilyMember
+end
+
+father = Father2.new
+eldest = Eldest2.new
+youngest = Youngest2.new
+
+def show_job(member)
+    member.job
+end 
+
+def show_chore(member)
+    member.chore
+end
+
+show_job(father)
+show_job(eldest)
+
+show_chore(youngest)
